8ed39e96ea3040d48c605254d3e30f37db44b30f
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-06-06 21:21:00 +0400

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new int[] { 2, 1, 5 };
+            SelectionSort.Sort(arr, (x, y) => x > y);
+        }
+    }
+}
---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class SelectionSort
+    {
+        public static void Sort<T>(T[] array, Func<T, T, bool> compare)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                var minIndex = i;
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (compare(array[minIndex], array[j]))
+                        minIndex = j;
+                }
+
+                var temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
