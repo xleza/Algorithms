bc6f835703d9490b10ead083e9fbbdb7a5d0d546
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-23 10:01:34 +0400

    add OddOccurrencesInArray

---
+++ Algorithms/OddOccurrencesInArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class OddOccurrencesInArray
+    {
+        public static int Execute(int[] arr)
+        {
+            Array.Sort(arr);
+            for (var i = 0; i < arr.Length - 1; i += 2)
+            {
+                if (arr[i] != arr[i + 1])
+                    return arr[i];
+            }
+
+            return arr[^1];
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,9 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //var r = CyclicRotation.Rotate(new[] {1, 2, 4}, 2);
-            //var binary = 2.ToBinary();
-            //BinaryMaxGap.Execute(9);
         }
     }
 }
