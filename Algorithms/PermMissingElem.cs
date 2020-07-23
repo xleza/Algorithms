dbe9c932736a0ddfa372683ee1f03b85115806c4
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-23 11:06:56 +0400

    add PermMissingElem

---
+++ Algorithms/PermMissingElem.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms
+{
+    public static class PermMissingElem
+    {
+        public static int Execute(int[] arr)
+        {
+            var aux = new int[arr.Length + 1];
+            foreach (var item in arr)
+            {
+                aux[item - 1] = item;
+            }
+
+            for (var i = 0; i < aux.Length; i++)
+            {
+                if (aux[i] == 0)
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
