8007904e9409639c6f4607d283afad3328e2bd1a
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-24 16:47:37 +0400

    add MaxCounters

---
+++ Algorithms/MaxCounters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class MaxCounters
+    {
+        public static int[] Count(int n, int[] arr)
+        {
+            var lastMax = 0;
+            var max = 0;
+            var result = new int[n];
+
+            foreach (var item in arr)
+            {
+                if (item == n + 1)
+                {
+                    lastMax = max;
+                    continue;
+                }
+
+                var index = item - 1;
+                result[index] = Math.Max(lastMax, result[index]);
+                result[index]++;
+                max = Math.Max(max, result[index]);
+            }
+
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = Math.Max(lastMax, result[i]);
+            }
+
+            return result;
+        }
+    }
+}
