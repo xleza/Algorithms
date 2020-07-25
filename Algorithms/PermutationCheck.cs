152fedf30ae2c5793efd3727582072a3b4a7d147
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-25 18:09:41 +0400

    add PermutationCheck

---
+++ Algorithms/PermutationCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Algorithms
+{
+    public sealed class PermutationCheck
+    {
+        public int Execute(int[] arr)
+        {
+            var aux = new int[arr.Length];
+            foreach (var item in arr)
+            {
+                var index = item - 1;
+                if (index > aux.Length - 1)
+                    return 0;
+
+                aux[index]++;
+            }
+
+            return aux.Any(item => item == 0) ? 0 : 1;
+        }
+    }
+}
