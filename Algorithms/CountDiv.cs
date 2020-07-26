4fef5dc52b418d3f16a4ff51658cce2558ddbf84
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-26 15:20:15 +0400

    add CountDiv

---
+++ Algorithms/CountDiv.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms
+{
+    public static class CountDiv
+    {
+        public static int Execute(int fromRange, int toRange, int divisor)
+        {
+            var offset = 0;
+            if (fromRange % divisor == 0)
+                ++offset;
+
+            return (fromRange / divisor) - (toRange / divisor) + offset;
+        }
+    }
+}
