e93e39ffc3854454cde02d319f18dd4d037a14d6
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-29 15:19:28 +0400

    MigratoryBirds

---
+++ Algorithms/MigratoryBirds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public sealed class MigratoryBirds
+    {
+        public static int GetResult(IEnumerable<int> birds)
+        {
+            var maxKind = int.MaxValue;
+            var maxKindValue = 0;
+            var kindsLookUp = new Dictionary<int, int>();
+
+            foreach (var bird in birds)
+            {
+                if (!kindsLookUp.ContainsKey(bird))
+                    kindsLookUp.Add(bird, 0);
+                kindsLookUp[bird]++;
+
+                if (kindsLookUp[bird] > maxKindValue)
+                {
+                    maxKind = bird;
+                    maxKindValue = kindsLookUp[bird];
+                }
+                else if (kindsLookUp[bird] == maxKindValue && maxKind > bird)
+                    maxKind = bird;
+            }
+
+            return maxKind;
+        }
+    }
+}
