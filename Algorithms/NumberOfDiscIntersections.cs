7e8eb1f9b33d8dd177281cb3c1d65fab648286b6
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-31 15:21:36 +0400

    NumberOfDiscIntersections

---
+++ Algorithms/NumberOfDiscIntersections.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class NumberOfDiscIntersections
+    {
+        private static int GetResult(int[] arr)
+        {
+            var openEndPoints = new long[arr.Length];
+            var closeEndPoints = new long[arr.Length];
+
+            for (long i = 0; i < arr.Length; i++)
+            {
+                openEndPoints[i] = i - arr[i];
+                closeEndPoints[i] = i + arr[i];
+            }
+
+            Array.Sort(openEndPoints);
+            Array.Sort(closeEndPoints);
+
+            var openEndPointIndex = 0;
+            var closeEndPointIndex = 0;
+            var openCircleCount = 0;
+            var result = 0;
+
+            while (openEndPointIndex <= arr.Length - 1)
+            {
+                if (openEndPoints[openEndPointIndex] <= closeEndPoints[closeEndPointIndex])
+                {
+                    result += openCircleCount;
+
+                    if (result > 10000000)
+                        return -1;
+
+                    openCircleCount++;
+                    openEndPointIndex++;
+                }
+                else
+                {
+                    openCircleCount--;
+                    closeEndPointIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
