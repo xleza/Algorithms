229c01a73ce06d68185c3f269ef50daaeb941b9b
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-07-31 17:17:33 +0400

    add shuffle

---
+++ Algorithms/Shuffle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Shuffle
+    {
+        private static void GetResult<T>(IList<T> collection)
+        {
+            var random = new Random();
+            for (var i = 0; i < collection.Count; i++)
+            {
+                var r = random.Next(0, i + 1);
+                var temp = collection[i];
+                collection[i] = collection[r];
+                collection[r] = temp;
+            }
+        }
+    }
+}
