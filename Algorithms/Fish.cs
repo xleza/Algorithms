ffa686a261c541eb3548196b0e67c91e46e7545e
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-01 15:24:12 +0400

    add Fish

---
+++ Algorithms/Fish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public static class Fish
+    {
+        public static int Execute(int[] A, int[] B)
+        {
+            var downStreams = new Stack<int>();
+            var eatenFishes = 0;
+            for (var i = 0; i < A.Length; i++)
+            {
+                if (B[i] == 1)
+                {
+                    downStreams.Push(i);
+                }
+                while (downStreams.Count != 0)
+                {
+                    eatenFishes++;
+                    var downStream = A[downStreams.Peek()];
+                    if (downStream > A[i])
+                    {
+                        break;
+                    }
+                    downStreams.Pop();
+                }
+            }
+            return A.Length - eatenFishes;
+        }
+    }
+}
