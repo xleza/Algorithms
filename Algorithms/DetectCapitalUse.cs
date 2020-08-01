2fa79ea7914f01e803666237676ac0b6816bb244
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-01 13:39:14 +0400

    add DetectCapitalUse

---
+++ Algorithms/DetectCapitalUse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public sealed class DetectCapitalUse
+    {
+        public static bool Execute(string word)
+        {
+            var firstIsCapital = char.IsUpper(word[0]);
+            var allCapitals = firstIsCapital;
+            var allNotCapitals = !firstIsCapital;
+
+            for (var i = 1; i < word.Length; i++)
+            {
+                if (char.IsUpper(word[i]))
+                {
+                    firstIsCapital = false;
+                    allNotCapitals = false;
+                }
+                else
+                {
+                    allCapitals = false;
+                }
+            }
+
+            return firstIsCapital || allNotCapitals || allCapitals;
+        }
+    }
+}
