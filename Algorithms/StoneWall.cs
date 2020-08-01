34bcb8e1e1ec24eacb117a6c0b7c7453076a5028
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-01 16:33:56 +0400

    add StoneWall

---
+++ Algorithms/StoneWall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public sealed class StoneWall
+    {
+        public int Execute(int[] wallHeights)
+        {
+            var blocks = new Stack<int>();
+            var result = 0;
+
+            foreach (var item in wallHeights)
+            {
+                while (blocks.Count > 0 && blocks.Peek() > item)
+                    blocks.Pop();
+
+                if (blocks.Count == 0)
+                {
+                    blocks.Push(item);
+                    result++;
+                    continue;
+                }
+
+                if (blocks.Peek() == item)
+                    continue;
+
+                blocks.Push(item);
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
