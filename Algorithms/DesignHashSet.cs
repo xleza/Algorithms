a9c83c02eccad3f2b62336fe473e3603c0924e3f
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-02 11:50:03 +0400

    add DesignHashSet

---
+++ Algorithms/DesignHashSet.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms
+{
+    public sealed class DesignHashSet
+    {
+        private readonly bool[] _elements = new bool[1000001];
+        public DesignHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+            _elements[key] = true;
+        }
+
+        public void Remove(int key)
+        {
+            _elements[key] = false;
+        }
+
+        public bool Contains(int key)
+        {
+            return _elements[key];
+        }
+    }
+}
