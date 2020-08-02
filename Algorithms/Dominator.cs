81778179718df6189d86bfd53733dc8435627215
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-02 12:02:25 +0400

    add Dominator

---
+++ Algorithms/Dominator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    public sealed class Dominator
+    {
+        public int Execute(int[] arr)
+        {
+            var element = arr.GroupBy(x => x)
+                .FirstOrDefault(x => x.Count() > arr.Length / 2);
+            if (element == null)
+                return -1;
+
+            return Array.IndexOf(arr, element.Key);
+        }
+    }
+}
