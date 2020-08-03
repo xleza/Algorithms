dc2540920c5a88956deedf852ad666f9cee1c622
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-03 12:10:30 +0400

    add IsPalindrome

---
+++ Algorithms/IsPalindrome.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms
+{
+    public static class IsPalindrome
+    {
+        public static bool Execute(string s)
+        {
+            var start = 0;
+            var end = s.Length - 1;
+            while (start < end)
+            {
+                if (!char.IsLetterOrDigit(s[start]))
+                {
+                    start++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(s[end]))
+                {
+                    end--;
+                    continue;
+                }
+
+                if (char.ToLower(s[start]) != char.ToLower(s[end]))
+                    return false;
+
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
