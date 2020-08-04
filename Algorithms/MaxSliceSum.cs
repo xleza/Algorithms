2301e3f7db4de089143703aba23c84165b733a93
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-04 18:48:47 +0400

    MaxSliceSum

---
+++ Algorithms/MaxSliceSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public sealed class MaxSliceSum
+    {
+        public static int Execute(int[] arr)
+        {
+            var sumEnding = 0;
+            var maxSum = int.MinValue;
+
+            foreach (var item in arr)
+            {
+                sumEnding = Math.Max(item, sumEnding + item);
+                maxSum = Math.Max(sumEnding, maxSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
