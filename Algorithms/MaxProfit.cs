58d217b8d0050ba5e6288b911575d1c41af8f94c
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-04 16:50:50 +0400

    MaxProfit

---
+++ Algorithms/MaxProfit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class MaxProfit
+    {
+        public static int Execute(int[] arr)
+        {
+            var result = 0;
+
+            if (arr.Length == 0)
+                return result;
+
+            var aux = new int[arr.Length];
+
+            var max = int.MinValue;
+            for (var i = arr.Length - 1; i >= 0; i--)
+            {
+                max = Math.Max(arr[i], max);
+                aux[i] = max;
+            }
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                result = Math.Max(aux[i] - arr[i], result);
+            }
+
+            return result;
+        }
+    }
+}
