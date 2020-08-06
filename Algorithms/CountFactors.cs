d88614897eb0795006998643629111c86c7e5b5c
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-06 14:29:49 +0400

    add CountFactors

---
+++ Algorithms/CountFactors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public sealed class CountFactors
+    {
+        public static int Execute(int n)
+        {
+            var result = 0;
+
+            for (var i = 1; i <= (int)Math.Sqrt(n); i++)
+            {
+                if (n % i != 0)
+                    continue;
+
+                result++;
+
+                if (n / i != i)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
