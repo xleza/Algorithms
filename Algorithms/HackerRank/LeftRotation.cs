efc9514152d04145da88a5177bf6a8ef738fed12
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-07 08:22:54 +0400

    add LeftRotation

---
+++ Algorithms/HackerRank/LeftRotation.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms.HackerRank
+{
+    public static class LeftRotation
+    {
+        public static int[] Execute(int[] arr, int d)
+        {
+            var aux = new int[arr.Length];
+            var rotations = d % arr.Length;
+            if (rotations == 0)
+                return arr;
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                var nextIndex = i - rotations;
+                if (nextIndex < 0)
+                    nextIndex = arr.Length + nextIndex;
+
+                aux[nextIndex] = arr[i];
+            }
+
+            return aux;
+        }
+    }
+}
