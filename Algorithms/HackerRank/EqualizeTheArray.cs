dd8b038deccf5433eb454b846ab79829df31c300
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-08 23:52:43 +0400

    EqualizeTheArray

---
+++ Algorithms/HackerRank/EqualizeTheArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.HackerRank
+{
+    public static class EqualizeTheArray
+    {
+        public static int Execute(int[] arr)
+        {
+            var list = arr.GroupBy(x => x).Select(x => x.Count());
+            var max = int.MinValue;
+            foreach (var item in list)
+            {
+                max = Math.Max(item, max);
+            }
+            return arr.Length - max;
+        }
+    }
+}
