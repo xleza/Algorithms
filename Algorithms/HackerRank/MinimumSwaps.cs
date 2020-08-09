5a394bd53aa898aa9baad659003f0a21dc638baa
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-09 14:48:07 +0400

    add minimumSwaps

---
+++ Algorithms/HackerRank/MinimumSwaps.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorithms.HackerRank
+{
+    public static class MinimumSwaps
+    {
+        public static int Execute(int[] arr)
+        {
+            var swaps = 0;
+            for (var i = 0; i < arr.Length;)
+            {
+                var inOrder = arr[i] == i + 1;
+                if (inOrder)
+                {
+                    i++;
+                    continue;
+                }
+
+                var newIndex = arr[i] - 1;
+                var temp = arr[newIndex];
+                arr[newIndex] = arr[i];
+                arr[i] = temp;
+                swaps++;
+            }
+
+            return swaps;
+        }
+    }
+}
