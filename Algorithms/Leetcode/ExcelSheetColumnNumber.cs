fdfc097a37264e85f8a043446508e496c9f1f84d
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-10 19:53:29 +0400

    add ExcelSheetColumnNumber

---
+++ Algorithms/Leetcode/ExcelSheetColumnNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms.Leetcode
+{
+    public sealed class ExcelSheetColumnNumber
+    {
+        public static int Execute(string sheet)
+        {
+            var result = 0;
+            for (var i = 0; i < sheet.Length; i++)
+            {
+                var numberInAlphabet = sheet[sheet.Length - 1 - i] - 64;
+                result += numberInAlphabet * (int)Math.Pow(26, i);
+            }
+
+            return result;
+        }
+    }
+}
