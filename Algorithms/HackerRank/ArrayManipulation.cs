a3e1d5d4949614ffd624f9504ee7a0e808e0b284
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-10 17:37:20 +0400

    add RansomNote and ArrayManipulation

---
+++ Algorithms/HackerRank/ArrayManipulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class ArrayManipulation
+    {
+        public static long Execute(int n, int[][] queries)
+        {
+            var arr = new long[n];
+            foreach (var query in queries)
+            {
+                var a = query[0] - 1;
+                var b = query[1];
+                var k = query[2];
+
+                arr[a] += k;
+                if (b < n)
+                    arr[b] -= k;
+            }
+
+            long sum = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                arr[i] = sum;
+            }
+
+            var max = long.MinValue;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                max = Math.Max(max, arr[i]);
+            }
+            return max;
+        }
+    }
+}
---
+++ Algorithms/HackerRank/RansomNote.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.HackerRank
+{
+    public static class RansomNote
+    {
+        public static bool Execute(string[] magazine, string[] note)
+        {
+            var magazineWordsLookUp = new Dictionary<string, int>();
+            foreach (var item in magazine)
+            {
+                if (!magazineWordsLookUp.ContainsKey(item))
+                    magazineWordsLookUp.Add(item, 0);
+
+                magazineWordsLookUp[item]++;
+            }
+
+            foreach (var item in note)
+            {
+                if (!magazineWordsLookUp.ContainsKey(item) || magazineWordsLookUp[item] == 0)
+                    return false;
+                magazineWordsLookUp[item]--;
+            }
+            return true;
+        }
+    }
+}
