d04b30d4fd45cb7bf747d1d3def8f7fd2a9d7664
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-12 19:36:48 +0400

    add PascalTriangle

---
+++ Algorithms/Leetcode/PascalTriangle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Leetcode
+{
+    public static class PascalTriangle
+    {
+        public static IList<IList<int>> Execute(int numRows)
+        {
+            var rows = new List<IList<int>>();
+            for (var i = 0; i < numRows; i++)
+            {
+                var row = new List<int>();
+                for (var j = 0; j <= i; j++)
+                {
+                    var hasPreviousRow = i > 0;
+                    var isEdge = !hasPreviousRow || j == 0 || j == i;
+                    if (isEdge)
+                    {
+                        row.Add(1);
+                        continue;
+                    }
+
+                    var previousRow = rows[i - 1];
+                    row.Add(previousRow[j - 1] + previousRow[j]);
+                }
+                rows.Add(row);
+            }
+
+            return rows;
+        }
+    }
+}
