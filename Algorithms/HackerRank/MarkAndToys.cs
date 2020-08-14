26ded972fefbd1187456c671666aa5c210a7cb34
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-14 15:20:15 +0400

    add MarkAndToys

---
+++ Algorithms/HackerRank/MarkAndToys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class MarkAndToys
+    {
+        public static int Execute(int[] prices, int k)
+        {
+            Array.Sort(prices);
+            var numberOfToys = 0;
+            var spent = 0;
+
+            foreach (var price in prices)
+            {
+                spent += price;
+                if (spent > k)
+                    break;
+                numberOfToys++;
+            }
+            return numberOfToys;
+        }
+    }
+}
