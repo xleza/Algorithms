2d0d74956f912549f6015545741c1f952bf6b682
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-16 16:40:35 +0400

    add SherlockAndValidString

---
+++ Algorithms/HackerRank/SherlockAndValidString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.HackerRank
+{
+    public static class SherlockAndValidString
+    {
+        public static bool Execute(string s)
+        {
+            var frequencies = new int[26];
+            foreach (var item in s)
+            {
+                frequencies[item - 'a']++;
+            }
+
+            var mostFrequentFrequency = frequencies.Where(x => x != 0).GroupBy(x => x).OrderBy(x => x.Count()).Last().Key;
+            var canUseDeletion = true;
+
+            for (var i = 0; i < frequencies.Length; i++)
+            {
+                if (frequencies[i] == mostFrequentFrequency || frequencies[i] == 0)
+                    continue;
+
+                if (!canUseDeletion)
+                    return false;
+
+                if (Math.Abs(frequencies[i] - mostFrequentFrequency) == 1 || frequencies[i] == 1)
+                    canUseDeletion = false;
+            }
+
+            return true;
+        }
+    }
+}
