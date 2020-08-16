a5d813c028d3296e92d3a012ad1f34f0d1ab9768
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-16 14:52:29 +0400

    add MakeAnagram

---
+++ Algorithms/HackerRank/MakeAnagram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class MakeAnagram
+    {
+        public static int Execute(string a, string b)
+        {
+            var frequencies = new int[26];
+            foreach (var item in a)
+                frequencies[item - 'a']++;
+
+            foreach (var item in b)
+                frequencies[item - 'a']--;
+
+            var deletions = 0;
+            foreach (var item in frequencies)
+                deletions += Math.Abs(item);
+
+            return deletions;
+        }
+    }
+}
