96b18e63bc3a501833be4345947d19a81c48ed94
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-17 16:01:43 +0400

    SpecialStringAgain

---
+++ Algorithms/HackerRank/SpecialStringAgain.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithms.HackerRank
+{
+    public static class SpecialStringAgain
+    {
+        public static long Execute(int n, string s)
+        {
+            long count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                var j = i + 1;
+                for (; j < n; j++)
+                {
+                    var equal = s[i] == s[j];
+                    if (equal)
+                    {
+                        count++;
+                        continue;
+                    }
+
+                    var end = j + (j - i) + 1;
+                    if (end > n)
+                        break;
+
+                    var areSame = true;
+                    for (var k = j + 1; k < end; k++)
+                    {
+                        if (s[k] != s[i])
+                            areSame = false;
+                    }
+
+                    if (areSame)
+                        count++;
+
+                    break;
+                }
+            }
+            return count + n;
+        }
+    }
+}
