629d42cc38d0367556f5d173a14cfd432bc0f2aa
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-18 14:33:45 +0400

    add MinimumAbsoluteDifference

---
+++ Algorithms/HackerRank/MinimumAbsoluteDifference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class MinimumAbsoluteDifference
+    {
+        public static int Execute(int[] arr)
+        {
+            Array.Sort(arr);
+            var min = int.MaxValue;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                min = Math.Min(min, Math.Abs(arr[i] - arr[i - 1]));
+            }
+            return min;
+        }
+    }
+}
