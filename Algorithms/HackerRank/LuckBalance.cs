96190fbe6ee1b8ce938d6d637900a4b77fec392d
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-18 15:05:06 +0400

    add LuckBalance

---
+++ Algorithms/HackerRank/LuckBalance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.HackerRank
+{
+    public static class LuckBalance
+    {
+        public static int Execute(int k, IEnumerable<int[]> contests)
+        {
+            var ordered = contests.OrderByDescending(x => x[0]);
+            var luckBalance = 0;
+            foreach (var contest in ordered)
+            {
+                if (contest[1] == 0)
+                    luckBalance += contest[0];
+                else if (k == 0)
+                    luckBalance -= contest[0];
+                else
+                {
+                    k--;
+                    luckBalance += contest[0];
+                }
+            }
+
+            return luckBalance;
+        }
+
+    }
+}
