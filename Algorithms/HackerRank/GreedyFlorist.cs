14a7c08595ed2126ffff161f7523c2486f1809e3
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-18 18:03:11 +0400

    add GreedyFlorist

---
+++ Algorithms/HackerRank/GreedyFlorist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class GreedyFlorist
+    {
+        public static int Execute(int n, int k, int[] c)
+        {
+            Array.Sort(c);
+            var totalPrice = 0;
+            for (var i = n - 1; i >= 0; i--)
+            {
+                totalPrice += ((n - 1 - i) / k + 1) * c[i];
+            }
+            return totalPrice;
+        }
+
+    }
+}
