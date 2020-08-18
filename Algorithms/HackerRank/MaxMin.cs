1de615b9475f2a09f1ecf246c6d755aae4084ff1
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-18 22:54:26 +0400

    add MaxMin

---
+++ Algorithms/HackerRank/MaxMin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Algorithms.HackerRank
+{
+    public static class MaxMin
+    {
+        public static int Execute(int k, int[] arr)
+        {
+            Array.Sort(arr);
+            var min = int.MaxValue;
+
+            for (var i = arr.Length - 1; i >= k - 1; i--)
+            {
+                min = Math.Min(min, arr[i] - arr[i - k + 1]);
+            }
+
+            return min;
+        }
+    }
+}
