82b4a92b019d814e29226716cf6fdb77e0c8b07e
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-19 11:41:46 +0400

    add WhatFlavors

---
+++ Algorithms/HackerRank/WhatFlavors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.HackerRank
+{
+    public sealed class WhatFlavors
+    {
+        public static void Execute(int[] cost, int money)
+        {
+            var costLookUp = new Dictionary<int, int>();
+            foreach (var item in cost)
+            {
+                if (!costLookUp.ContainsKey(item))
+                    costLookUp.Add(item, 0);
+                costLookUp[item]++;
+            }
+
+            for (var i = 0; i < cost.Length; i++)
+            {
+                var firstCost = cost[i];
+                var secondCost = money - cost[i];
+
+                costLookUp[firstCost]--;
+
+                if (!costLookUp.ContainsKey(secondCost) || costLookUp[secondCost] == 0)
+                    continue;
+
+                var firstFlavor = i + 1;
+                var secondFlavor = -1;
+                for (; i < cost.Length; i++)
+                {
+                    if (cost[i] == secondCost)
+                        secondFlavor = i + 1;
+                }
+
+                Console.WriteLine(firstFlavor + " " + secondFlavor);
+            }
+        }
+    }
+}
