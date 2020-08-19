0b6a57184d6c7f96e538cacf269367f8479a8dd1
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-19 12:34:19 +0400

    add pairs

---
+++ Algorithms/HackerRank/Pairs.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Algorithms.HackerRank
+{
+    public sealed class Pairs
+    {
+        static int Execute(int k, int[] arr)
+        {
+            var set = arr.ToHashSet();
+            var count = 0;
+            foreach (var item in arr)
+            {
+                if (set.Contains(item - k))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
