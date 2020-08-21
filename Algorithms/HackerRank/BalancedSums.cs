44d6a132f7a01b3e590f93162aa8f9d0142f5929
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-21 10:21:25 +0400

    add BalancedSums

---
+++ Algorithms/HackerRank/BalancedSums.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.HackerRank
+{
+    public static class BalancedSums
+    {
+        public static bool Execute(List<int> items)
+        {
+            var prefixSums = new int[items.Count];
+            var sum = 0;
+            for (var i = 0; i < items.Count; i++)
+            {
+                sum += items[i];
+                prefixSums[i] = sum;
+            }
+
+            for (var i = 0; i < prefixSums.Length; i++)
+            {
+                if (prefixSums[i] - items[i] == prefixSums[prefixSums.Length - 1] - prefixSums[i])
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
