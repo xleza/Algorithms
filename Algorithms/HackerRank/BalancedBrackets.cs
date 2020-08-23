7c7445756c681c8b129bc1382261bf6b0e4a6729
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-23 16:10:52 +0400

    add BalancedBrackets

---
+++ Algorithms/HackerRank/BalancedBrackets.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.HackerRank
+{
+    public static class BalancedBrackets
+    {
+        public static string Execute(string s)
+        {
+            var bracketsPairs = new Dictionary<char, char>{
+                {'(' , ')'},
+                {'{' , '}'},
+                {'[' , ']'}
+            };
+            var openBrackets = new Stack<char>();
+            foreach (var item in s)
+            {
+                if (bracketsPairs.ContainsKey(item))
+                    openBrackets.Push(item);
+                else
+                {
+                    if (openBrackets.Count == 0)
+                        return "NO";
+                    var openBracket = openBrackets.Pop();
+                    if (bracketsPairs[openBracket] != item)
+                        return "NO";
+                }
+            }
+            return openBrackets.Count == 0 ? "YES" : "NO";
+        }
+    }
+}
