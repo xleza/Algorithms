5ae9278428c5c3168718458622e0d50e07757700
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-23 18:36:37 +0400

    add CustomQueue

---
+++ Algorithms/HackerRank/CustomQueue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.HackerRank
+{
+    public sealed class CustomQueue
+    {
+        private Stack<int> _lifo;
+        private Stack<int> _fifo;
+
+        public CustomQueue()
+        {
+            _lifo = new Stack<int>();
+            _fifo = new Stack<int>();
+        }
+
+        public void Enqueue(int item)
+        {
+            _lifo.Push(item);
+        }
+
+        public void Dequeue()
+        {
+            if (_fifo.Count == 0)
+            {
+                while (_lifo.Count > 0)
+                    _fifo.Push(_lifo.Pop());
+            }
+
+            _fifo.Pop();
+        }
+
+
+        public int Peek()
+        {
+            if (_fifo.Count == 0)
+            {
+                while (_lifo.Count > 0)
+                    _fifo.Push(_lifo.Pop());
+            }
+
+            return _fifo.Peek();
+        }
+    }
+}
