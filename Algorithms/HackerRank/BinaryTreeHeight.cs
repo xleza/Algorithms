1b9212ccd2e406cb295a1a543cdf217bc2bac385
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-08-28 20:50:46 +0400

    add PreorderTraversal

---
+++ Algorithms/HackerRank/BinaryTreeHeight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.HackerRank
+{
+    public static class BinaryTreeHeight
+    {
+        static int Height(Node root)
+        {
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+            var height = 0;
+
+            while (true)
+            {
+                var nodeCountOnLevel = queue.Count;
+                if (nodeCountOnLevel == 0)
+                    break;
+
+                height++;
+
+                for (var i = 0; i < nodeCountOnLevel; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.Left != null)
+                        queue.Enqueue(node.Left);
+                    if (node.Right != null)
+                        queue.Enqueue(node.Right);
+                }
+            }
+
+            return height;
+        }
+
+
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+    }
+}
---
+++ Algorithms/Leetcode/PreorderTraversal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Leetcode
+{
+    public static class PreorderTraversal
+    {
+        public class TreeNode
+        {
+            public int Val;
+            public TreeNode Left;
+            public TreeNode Right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                Val = val;
+                Left = left;
+                Right = right;
+            }
+
+            public static IList<int> Execute(TreeNode root)
+            {
+                var traversedNodes = new List<int>();
+
+                if (root == null)
+                    return traversedNodes;
+
+                var visited = new HashSet<TreeNode>();
+                var stack = new Stack<TreeNode>();
+
+                stack.Push(root);
+
+                while (stack.Count != 0)
+                {
+                    var node = stack.Peek();
+                    if (visited.Contains(node))
+                    {
+                        stack.Pop();
+
+                        if (node.Right != null)
+                            stack.Push(node.Right);
+
+                        continue;
+                    }
+
+                    traversedNodes.Add(node.Val);
+                    visited.Add(node);
+
+                    if (node.Left != null)
+                        stack.Push(node.Left);
+                }
+
+                return traversedNodes;
+            }
+        }
+    }
+}
