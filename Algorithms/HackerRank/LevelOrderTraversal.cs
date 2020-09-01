d3406c4de0644c198cc52c077d12b2cb89347be7
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-09-01 08:43:34 +0400

    add LevelOrderTraversal

---
+++ Algorithms/HackerRank/LevelOrderTraversal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.HackerRank
+{
+    public static class LevelOrderTraversal
+    {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public static List<int> Execute(Node root)
+        {
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            var traversedValues = new List<int>();
+
+            while (queue.Count != 0)
+            {
+                var nodesCount = queue.Count;
+                while (nodesCount != 0)
+                {
+                    var node = queue.Dequeue();
+                    traversedValues.Add(node.Data);
+                    if (node.Left != null)
+                    {
+                        queue.Enqueue(node.Left);
+                    }
+                    if (node.Right != null)
+                    {
+                        queue.Enqueue(node.Right);
+                    }
+                    nodesCount--;
+                }
+            }
+
+            return traversedValues;
+        }
+    }
+}
