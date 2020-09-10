fc6487a28cef6afa7c76fee059561313ba136b6f
g.sultanishvili@altasoft.ge <g.sultanishvili@altasoft.ge>
2020-09-10 13:09:00 +0400

    add BullsAndCows

---
+++ Algorithms/Leetcode/BullsAndCows.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Leetcode
+{
+    public static class BullsAndCows
+    {
+        public static string Execute(string secret, string guess)
+        {
+            var numberLookUp = new Dictionary<int, int>();
+            foreach (var item in secret)
+            {
+                if (!numberLookUp.ContainsKey(item))
+                    numberLookUp.Add(item, 0);
+
+                numberLookUp[item]++;
+            }
+
+            var cows = 0;
+            var bulls = 0;
+
+            for (var i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] != guess[i])
+                    continue;
+
+                bulls++;
+
+                numberLookUp[guess[i]]--;
+            }
+
+            for (var i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] == guess[i])
+                    continue;
+
+                if (!numberLookUp.ContainsKey(guess[i]) || numberLookUp[guess[i]] <= 0)
+                    continue;
+
+                cows++;
+
+                numberLookUp[guess[i]]--;
+            }
+
+            return $"{bulls}A{cows}B";
+        }
+    }
+}
